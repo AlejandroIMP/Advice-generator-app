e3e05a3920c4c4324c2a50805eb1536b941b5fa2
AlejandroIMP <alejandrosunuc@gmail.com>
2024-08-22 12:49:11 -0600

    Bugfix fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,11 @@ function App () {
       })
   }, []
   )
-  const fetchRandomQuote = () => {
-    fetch('https://api.adviceslip.com/advice')
-      .then(response => response.json())
-      .then(data => {
-        setQuote(data.slip.advice)
-        setId(data.slip.id)
-      })
+  const fetchRandomQuote = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const data = await response.json()
+    setQuote(data.slip.advice)
+    setId(data.slip.id)
   }
   return (
     <main>
